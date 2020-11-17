eb841c81d231601b3e49ae8fab811eb94d11b539
xoliio <xoliio@gmail.com>
2020-11-16 19:25:58 -0800

    with Jesse and Michael

--- index.js
+++ index.js
@@ -10,4 +10,21 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(string) {
+  let time = string.split(":").join("");
+  if (time < 1200) {
+    return "Good Morning"
+  }
+  if (time >= 1200 && time < 1700) {
+    return "Good Afternoon"
+  }
+  if (time >= 1700) {
+    return "Good Evening"
+  }
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(string) {
+  let greeting = document.querySelector("#greeting")
+  greeting.innerText = string
+}
